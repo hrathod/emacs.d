5cca9a583f121c067b20627adbad7e4050c338e0
hrathod <>
2017-01-20 11:23:33 -0500

    Update package repository list.

--- init.el
+++ init.el
@@ -11,8 +11,8 @@
 (require 'package)
 
 ;;; CODE:
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
 (when (not package-archive-contents)

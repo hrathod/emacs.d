032119e330669f1bf6ef13c781ca95f467089c43
hrathod <>
2017-01-25 11:21:02 -0500

    Use use-package to load libraries.

--- init.el
+++ init.el
@@ -1,40 +1,270 @@
 ;;; init -- personal config
 ;;; Commentary:
 ;;; Personal config
-;;; LOAD PATH
-(add-to-list 'load-path "~/.emacs.d/custom/smartparens-js/")
-(let ((default-directory (concat user-emacs-directory
-                                 (convert-standard-filename "custom/"))))
-  (normal-top-level-add-subdirs-to-load-path))
 
-;;; PACKAGES
+;;; Code:
 (require 'package)
-
-;;; CODE:
+(setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
-(when (not package-archive-contents)
-  (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-(defvar package-list)
-(setq-default package-list
-              '(flycheck smartparens js2-mode js2-refactor js-doc web-mode
-                         expand-region clojure-mode exec-path-from-shell
-                         projectile undo-tree company cider
-                         clojure-mode-extra-font-locking clj-refactor
-                         magit diminish less-css-mode anzu clojure-snippets
-                         diff-hl helm helm-flycheck helm-projectile
-                         helm-descbinds go-mode company-go company-tern
-                         json-mode ace-window helm-ag helm-git-grep))
 
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
+(use-package midnight
+  :ensure t)
+
+(use-package flycheck
+  :ensure t)
+
+(use-package which-key
+  :ensure t)
+
+(use-package ace-window
+  :ensure t
+  :init (setq aw-keys '(?a ?s ?d ?f ?j ?k ?l))
+  (setq aw-scope 'frame)
+  :bind (("C-x 0" . ace-window)
+         ("C-c w d" . ace-delete-window)))
+
+(use-package anzu
+  :ensure t
+  :config (global-anzu-mode)
+  :bind (("M-%" . anzu-query-replace)
+         ("C-M-%" . anzu-query-replace-regexp)))
+
+(use-package ediff
+  :ensure t
+  :init (setq ediff-window-setup-function 'ediff-setup-windows-plain))
+
+(use-package diff-hl
+  :ensure t
+  :init (setq diff-hl-side 'right)
+  (setq diff-hl-draw-borders t)
+  :config (global-diff-hl-mode +1))
+
+(use-package company
+  :ensure t
+  :init (setq company-idle-delay 0.5)
+  (setq company-tooltip-limit 10)
+  (setq company-minimum-prefix-length 1)
+  (setq company-tooltip-flip-when-above t)
+  :config (global-company-mode 1))
+
+(use-package undo-tree
+  :ensure t
+  :init (setq undo-tree-history-directory-alist `((".*" . ,temporary-file-directory)))
+  (setq undo-tree-auto-save-history t)
+  :config (global-undo-tree-mode t))
+
+(use-package helm
+  :ensure t
+  :init (require 'helm-config)
+  :config (helm-mode 1)
+  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+  (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
+  :bind (("C-c h" . helm-command-prefix)
+         ("C-c h h" . helm-mini)
+         ("C-c h x" . helm-M-x)
+         ("C-c h b" . helm-buffers-list)
+         ("C-x C-f" . helm-find-files)
+         ("C-c h w" . helm-google-suggest)
+         ("C-c h o" . helm-occur)
+         ))
+
+(use-package helm-swoop
+  :ensure t
+  :bind (("C-c h s" . helm-swoop)))
+
+(use-package helm-git-grep
+  :ensure t
+  :bind ("C-c h g" . helm-git-grep))
+
+(use-package helm-descbinds
+  :ensure t
+  :bind (("C-x w" . helm-descbinds)))
+
+(use-package helm-flycheck
+  :ensure t
+  :bind (("C-c h e" . helm-flycheck)))
+
+(use-package helm-ag
+  :ensure t
+  :bind (("C-c h a" . helm-ag)))
+
+(use-package expand-region
+  :ensure t
+  :bind
+  (("C-=" . er/expand-region)
+   ("C--" . er/contract-region)))
+
+(use-package whitespace
+  :ensure t
+  :init
+  (setq whitespace-line-column 120)
+  (setq whitespace-style '(face tabs empty trailing lines-trail))
+  :config
+  (global-whitespace-mode))
+
+(use-package magit
+  :ensure t
+  :bind (("C-c g" . magit-status)))
+
+(use-package projectile
+  :ensure t
+  :init
+  (progn
+    (setq projectile-completion-system 'helm)
+    (setq projectile-enable-caching t))
+  :config
+  (progn
+    (add-to-list 'projectile-globally-ignored-directories "node_modules")
+    (projectile-mode 1)))
+
+;;;(use-package helm-projectile
+;;;             :ensure t
+;;;             :init (setq projectile-completion-system 'helm)
+;;;                   ;;;(setq projectile-enable-caching t)
+;;;             :config (helm-projectile-on))
+
+(use-package yasnippet
+  :ensure t
+  :config (yas-global-mode 1))
+
+(use-package exec-path-from-shell
+  :ensure t
+  :config (exec-path-from-shell-initialize))
+
+(use-package web-mode
+  :ensure t
+  :init
+  (progn
+    (setq web-mode-markup-indent-offset 4)
+    (setq web-mode-css-indent-offset 4)
+    (setq web-mode-code-indent-offset 4)
+    (setq web-mode-indent-style 4)
+    (setq web-mode-enable-auto-pairing nil))
+  :config
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
+
+(use-package flycheck
+  :ensure t
+  :init (setq flycheck-disabled-checkers '(javascript-jscs javascript-jshint))
+  :config (add-hook 'after-init-hook #'global-flycheck-mode))
+
+(use-package js2-mode
+  :ensure t
+  :init
+  (progn
+    (setq js2-indent-switch-body t)
+    (setq js2-bounce-indent-p t)
+    (setq js2-global-externs '("module" "require" "sinon" "assert"
+                               "setTimeout" "clearTimeout" "setInterval"
+                               "clearInterval" "console" "JSON"
+                               "describe" "it" "before" "beforeEach"
+                               "after" "afterEach" "process" "Buffer")))
+  :functions flycheck-select-checker
+  :config
+  (progn
+    (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+    (add-hook 'js2-mode-hook (lambda ()
+                               (flycheck-select-checker 'javascript-eslint)))))
+
+(use-package company-tern
+  :ensure t
+  :config
+  (progn
+    (add-to-list 'company-backends 'company-tern)
+    (add-hook 'js2-mode-hook (lambda ()
+                               (tern-mode 1)))))
+
+(use-package json-mode
+  :ensure t)
+
+(use-package js-doc
+  :ensure t
+  :init
+  (define-key js2-mode-map (kbd "C-c * f") 'js-doc-insert-function-doc)
+  (define-key js2-mode-map (kbd "C-c * t") 'js-doc-insert-tag))
+
+(use-package js2-refactor
+  :ensure t
+  :config
+  (progn
+    (js2r-add-keybindings-with-prefix "C-c r")
+    (add-hook 'js2-mode-hook #'js2-refactor-mode)))
+
+(use-package clojure-mode
+  :ensure t
+  :functions define-clojure-indent
+  :config
+  (define-clojure-indent
+    (defroutes 'defun)
+    (GET 2)
+    (POST 2)
+    (PUT 2)
+    (DELETE 2)
+    (HEAD 2)
+    (ANY 2)
+    (context 2)))
+
+(use-package clj-refactor
+  :ensure t
+  :functions cljr-add-keybindings-with-prefix
+  :init
+  (progn
+    (cljr-add-keybindings-with-prefix "C-c r")
+    (add-hook 'clojure-mode-hook (lambda ()
+                                   (clj-refactor-mode 1)))))
+
+(use-package cider
+  :ensure t)
+
+(use-package clojure-mode-extra-font-locking
+  :ensure t)
+
+(use-package clojure-snippets
+  :ensure t)
+
+(use-package go-mode
+  :ensure t)
+
+(use-package company-go
+  :ensure t
+  :init (setenv "GOPATH" (substitute-in-file-name "$HOME/Proejcts/Go"))
+  :config
+  (add-hook 'go-mode-hook
+            (lambda ()
+              (add-hook 'before-save-hook 'gofmt-before-save)
+              (if (not (string-match "go" compile-command))
+                  (set (make-local-variable 'compile-command)
+                       "go generate && go build -v && go test -v && go vet"))
+              (local-set-key (kbd "M-.") 'godef-jump))))
+
+(use-package smartparens
+  :ensure t
+  :init
+  (require 'smartparens-config)
+  (progn
+    (setq sp-base-key-bindings 'paredit)
+    (setq sp-autoskip-closing-pair 'always)
+    (setq sp-hybrid-kill-entire-symbol nil))
+  :config
+  (progn
+    (smartparens-strict-mode +1)
+    (smartparens-global-strict-mode +1)
+    (show-smartparens-global-mode +1)
+    (sp-use-paredit-bindings)
+    (turn-on-smartparens-strict-mode)))
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
 
 ;;; convenience
-(require 'midnight) ; clean up obsolete buffers
 (setq inhibit-startup-message t)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -51,32 +281,6 @@
   (invert-face 'mode-line)
   (run-with-timer 0.1 nil 'invert-face 'mode-line))
 (setq ring-bell-function 'my-visible-bell)
-
-;;; Windows
-(require 'ace-window)
-(global-set-key (kbd "C-x o") 'ace-window)
-(global-set-key (kbd "C-c w d") 'ace-delete-window)
-
-(setq-default aw-keys '(?a ?s ?d ?f ?j ?k ?l))
-(setq-default aw-scope 'frame)
-;;;(setq-default aw-background 1)
-
-;;; Search & Replace
-(require 'anzu)
-(global-anzu-mode)
-(global-set-key (kbd "M-%") 'anzu-query-replace)
-(global-set-key (kbd "C-M-%") 'anzu-query-replace-regexp)
-
-;;; Diffing
-(require 'ediff)
-(setq ediff-window-setup-function  'ediff-setup-windows-plain)
-
-;;; Git changes
-(require 'diff-hl)
-(global-diff-hl-mode +1)
-(setq-default diff-hl-side 'right)
-(setq-default diff-hl-draw-borders nil)
-(add-hook 'dired-mode-hook 'diff-hl-dired-mode)
 
 ;;; Movement (from Prelude)
 (defun my-beginning-of-line (arg)
@@ -96,194 +300,6 @@
 (global-set-key [remap move-beginning-of-line]
                 'my-beginning-of-line)
 
-
-;;; Look & Feel
-(setq default-frame-alist
-      '((font . "Source Code Pro-13")
-        ;;(foreground-color . "#d9d9d9")
-        ;;(background-color . "#353535")
-        (background-color . "#fdfdfd")
-        (foreground-color . "#353535")
-        ;;(border-color . "#353535")
-        (border-color . "#fdfdfd")
-        ;;(scroll-bar-background . "#353535")
-        ;;(scroll-bar-foreground . "#555555")
-        (cursor-color . "#339955")
-        (line-spacing . 3)
-        (vertical-scroll-bars . nil)
-        (left-fringe . 8)
-        (right-fringe . 8)))
-(setq-default left-margin-width 2)
-(setq-default right-margin-width 2)
-(set-face-attribute 'region nil :background "#777777")
-(set-face-attribute 'fringe nil :background "#fdfdfd")
-
-;;; Auto Complete
-(require 'company)
-(setq company-idle-delay 0)
-(setq company-tooltip-limit 10)
-;;;(setq company-minimum-prefix-length 2)
-(setq-default company-minimum-prefix-length 0)
-(setq company-tooltip-flip-when-above t)
-(global-company-mode 1)
-
-
-;;; UNDO
-(require 'undo-tree)
-(global-undo-tree-mode t)
-(setq undo-tree-history-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq undo-tree-auto-save-history t)
-
-;;; BACKUPS
-(setq-default create-lockfiles nil)
-(setq auto-save-interval 125)
-(setq make-backup-files t
-      backup-by-copying t
-      delete-old-versions t
-      kept-new-versions 6
-      kept-old-versions 2
-      version-control 1
-      vc-make-backup-files t
-      vc-follow-symlinks t)
-;; store all backup and autosave files in the tmp dir
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
-
-
-;;; Helm
-(require 'helm)
-(require 'helm-config)
-(require 'helm-flycheck)
-(require 'helm-descbinds)
-(require 'helm-ag)
-(global-set-key (kbd "C-c h") 'helm-command-prefix)
-(global-set-key (kbd "C-c h x") 'helm-M-x)
-(global-set-key (kbd "C-c h e") 'helm-flycheck)
-(global-set-key (kbd "C-c h w") 'helm-google-suggest)
-(global-set-key (kbd "C-c h s") 'helm-ag)
-(global-set-key (kbd "C-c h g") 'helm-git-grep)
-(global-set-key (kbd "C-c h o") 'helm-occur)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
-(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
-(helm-mode 1)
-
-;;; Fix PATH
-(require 'exec-path-from-shell)
-(exec-path-from-shell-initialize)
-
-;;; General
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq-default company-dabbrev-downcase nil)
-(setq-default insert-directory-program "/usr/local/bin/gls")
-(global-auto-revert-mode t)
-
-;;; Region expansion
-(require 'expand-region)
-(global-set-key (kbd "C-=") 'er/expand-region)
-(global-set-key (kbd "C--") 'er/contract-region)
-
-;;; Whitespace
-(require 'whitespace)
-(global-whitespace-mode)
-(setq whitespace-line-column 120)
-(setq whitespace-style '(face tabs empty trailing lines-tail))
-
-;;; GIT
-(require 'magit)
-(global-set-key (kbd "C-c g") 'magit-status)
-
-;;; Projects
-(require 'projectile)
-(require 'helm-projectile)
-(setq projectile-completion-system 'helm)
-(helm-projectile-on)
-(projectile-global-mode)
-
-;;; Snippets
-(yas-global-mode 1)
-
-;;; Web
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(setq-default web-mode-markup-indent-offset 4)
-(setq-default web-mode-css-indent-offset 4)
-(setq-default web-mode-code-indent-offset 4)
-(setq-default web-mode-indent-style 4)
-(setq-default web-mode-enable-auto-pairing nil)
-
-;;; Flycheck
-(require 'flycheck)
-(add-hook 'after-init-hook #'global-flycheck-mode)
-(setq-default flycheck-disabled-checkers '(javascript-jscs javascript-jshint))
-
-;;; JS
-(require 'js2-mode)
-(require 'js2-refactor)
-(require 'js-doc)
-(require 'company-tern)
-(require 'json-mode)
-(add-to-list 'company-backends 'company-tern)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(setq-default js2-indent-switch-body t)
-(setq-default js2-global-externs '("module" "require" "sinon" "assert"
-                                   "setTimeout" "clearTimeout" "setInterval"
-                                   "clearInterval" "console" "JSON"
-                                   "describe" "it" "before" "beforeEach"
-                                   "after" "afterEach" "process" "Buffer"))
-(with-eval-after-load 'flycheck
-  (setq flycheck-disabled-checkers
-        (append flycheck-disabled-checkers
-                '(javascript-jshint javascript-jscs))))
-(add-hook 'js2-mode-hook #'js2-refactor-mode)
-(add-hook 'js2-mode-hook
-          (lambda ()
-            (tern-mode 1)
-            ;;;(set (make-local-variable 'company-backends) '((company-tern company-yasnippet company-dabbrev-code)))
-            (setq-default js2-bounce-indent t)
-            (js2r-add-keybindings-with-prefix "C-c r")
-            (define-key js2-mode-map (kbd "C-c * f") 'js-doc-insert-function-doc)
-            (define-key js2-mode-map (kbd "C-c * t") 'js-doc-insert-tag)
-            (flycheck-select-checker 'javascript-eslint)))
-
-;;; Clojure
-(require 'clojure-mode)
-(require 'clj-refactor)
-
-(define-clojure-indent
-  (defroutes 'defun)
-  (GET 2)
-  (POST 2)
-  (PUT 2)
-  (DELETE 2)
-  (HEAD 2)
-  (ANY 2)
-  (context 2))
-
-(defun my-clojure-mode-hook ()
-  "Custom clojure mode hook."
-  (clj-refactor-mode 1)
-  (yas-minor-mode 1)
-  (cljr-add-keybindings-with-prefix "C-c r"))
-(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
-
-;;; Go-lang
-(require 'company)
-(require 'company-go)
-(setenv "GOPATH" (substitute-in-file-name "$HOME/Projects/Go"))
-(add-hook 'go-mode-hook
-          (lambda ()
-            (set (make-local-variable 'company-backends) '((company-go company-dabbrev-code company-yasnippet)))
-            (add-hook 'before-save-hook 'gofmt-before-save)
-            (if (not (string-match "go" compile-command))
-                (set (make-local-variable 'compile-command)
-                     "go generate && go build -v && go test -v && go vet"))
-            (local-set-key (kbd "M-.") 'godef-jump)))
-
 ;;; Code cleanup
 (defun code-cleanup ()
   "Clean up current buffer."
@@ -294,27 +310,28 @@
   (untabify (point-min) (point-max)))
 (define-key prog-mode-map (kbd "C-S-f") 'code-cleanup)
 
-;;; PARENS
-(require 'smartparens)
-(require 'smartparens-config)
-(require 'smartparens-js)
-(setq sp-base-key-bindings 'paredit)
-(setq sp-autoskip-closing-pair 'always)
-(setq sp-hybrid-kill-entire-symbol nil)
-(smartparens-strict-mode +1)
-(smartparens-global-strict-mode +1)
-(show-smartparens-global-mode +1)
-(sp-use-paredit-bindings)
-(turn-on-smartparens-strict-mode)
+;;; Look & Feel
+(setq default-frame-alist
+      '((font . "Source Code Pro-15")
+        (foreground-color . "#d9d9d9")
+        (background-color . "#353535")
+        (scroll-bar-background . "#353535")
+        (scroll-bar-foreground . "#555555")
+        (border-color . "#353535")
+        (line-spacing . 3)
+        (vertical-scroll-bars . nil)
+        (left-fringe . 8)
+        (right-fringe . 8)))
+(set-face-attribute 'region nil :background "#dffdfd" :foreground "#000000")
+(set-face-attribute 'fringe nil :background "#353535")
+(setq-default left-margin-width 2)
+(setq-default right-margin-width 2)
 
-;;; Modeline
-(when (require 'diminish)
-  (diminish 'undo-tree-mode)
-  (diminish 'yas-global-mode)
-  (diminish 'company-mode)
-  (diminish 'abbrev-mode)
-  (diminish 'anzu-mode)
-  (diminish 'helm-mode))
+;;; General
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq-default insert-directory-program "/usr/local/bin/gls")
+(global-auto-revert-mode t)
 
 (provide 'init)
 ;;; init ends here

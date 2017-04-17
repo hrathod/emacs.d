fafe9b82cee40c9d864b1ee410fd90bfa9687bba
hrathod <>
2017-04-17 12:03:59 -0400

    Fix undo-tree config error

--- init.el
+++ init.el
@@ -57,9 +57,11 @@
 (use-package undo-tree
   :ensure t
   :diminish undo-tree-mode
-  :init (setq undo-tree-history-directory-alist `((".*" . ,temporary-file-directory)))
-  (setq undo-tree-auto-save-history t)
-  :config (global-undo-tree-mode t))
+  :config
+  (progn
+    (setq undo-tree-history-directory-alist `((".*" . ,temporary-file-directory)))
+    (setq undo-tree-auto-save-history t)
+    (global-undo-tree-mode t)))
 
 (use-package helm
   :ensure t
